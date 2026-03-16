14741a17a590df1f8e7f191db6a29b143222d4c4
Pavel1771123 <pavelklimko@gmail.com>
2026-03-16 21:49:52 +0300

    Revert DB check and add missing logs directory

--- des_nat/settings/base.py
+++ des_nat/settings/base.py
@@ -76,24 +76,17 @@ WSGI_APPLICATION = 'des_nat.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # SQLite (commented out, use PostgreSQL instead)
+# PostgreSQL configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME', default='nataliya_db'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
-
-# PostgreSQL configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': config('DB_NAME', default='nataliya_db'),
-#         'USER': config('DB_USER', default='postgres'),
-#         'PASSWORD': config('DB_PASSWORD', default='postgres'),
-#         'HOST': config('DB_HOST', default='localhost'),
-#         'PORT': config('DB_PORT', default='5432'),
-#     }
-# }
 
 
 # Password validation
